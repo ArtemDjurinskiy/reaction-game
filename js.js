165cf2d9c1e3ece1a24cdddd62a7c298a80b926c
Artemka <Artemdjurinksiy@gmail.com>
2019-10-19 21:24:58 +0300

    first game

---
+++ js.js
@@ -0,0 +1,45 @@
+var start = new Date().getTime();
+        
+        function element(id){
+            return document.getElementById(id);
+        }
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+        function makeElementAppear(){
+            var top = Math.random() * 400;
+            var left = Math.random() * 1000;
+            var width = (Math.random() * 100) + 100;
+
+            if (Math.random() > 0.5) {
+                element('element').style.borderRadius = "50%";                
+            } else {
+                element('element').style.borderRadius = "0%";                
+
+            }
+            element('element').style.marginTop = top + "px";
+            element('element').style.marginLeft = left + "px";
+            element('element').style.width = width + "px";
+            element('element').style.height = width + "px";
+            element('element').style.display = "block";
+            element('element').style.backgroundColor = getRandomColor();
+            start = new Date().getTime();
+        }
+
+        function apperAfterDelete(){
+        setTimeout(makeElementAppear, Math.random() *  2000);
+
+        }
+        apperAfterDelete();
+        element('element').onclick = function(){
+            var end = new Date().getTime();            
+            var timeTaken = (end -start) /1000;
+            element('element').style.display = "none";
+            element('timeTaken').innerHTML = timeTaken + " S.";
+            apperAfterDelete();            
+        };
