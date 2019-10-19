634d75a6a1f693118b70de198f3a83a22954e9a5
Artemka <Artemdjurinksiy@gmail.com>
2019-10-19 22:37:08 +0300

    variable and condition added

--- js.js
+++ js.js
@@ -1,45 +1,52 @@
 var start = new Date().getTime();
-        
-        function element(id){
-            return document.getElementById(id);
-        }
-        function getRandomColor() {
-            var letters = '0123456789ABCDEF';
-            var color = '#';
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
-        function makeElementAppear(){
-            var top = Math.random() * 400;
-            var left = Math.random() * 1000;
-            var width = (Math.random() * 100) + 100;
+var bestTime = 0;
 
-            if (Math.random() > 0.5) {
-                element('element').style.borderRadius = "50%";                
-            } else {
-                element('element').style.borderRadius = "0%";                
+function element(id){
+    return document.getElementById(id);
+};
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+function makeElementAppear(){
+    var top = Math.random() * 400;
+    var left = Math.random() * 1000;
+    var width = (Math.random() * 100) + 100;
 
-            }
-            element('element').style.marginTop = top + "px";
-            element('element').style.marginLeft = left + "px";
-            element('element').style.width = width + "px";
-            element('element').style.height = width + "px";
-            element('element').style.display = "block";
-            element('element').style.backgroundColor = getRandomColor();
-            start = new Date().getTime();
-        }
+    if (Math.random() > 0.5) {
+        element('element').style.borderRadius = "50%";                
+    } else {
+        element('element').style.borderRadius = "0%";
+    }
+    element('element').style.marginTop = top + "px";
+    element('element').style.marginLeft = left + "px";
+    element('element').style.width = width + "px";
+    element('element').style.height = width + "px";
+    element('element').style.display = "block";
+    element('element').style.backgroundColor = getRandomColor();
+    start = new Date().getTime();
+};
 
-        function apperAfterDelete(){
-        setTimeout(makeElementAppear, Math.random() *  2000);
+function apperAfterDelete(){
+setTimeout(makeElementAppear, Math.random() *  2000);
 
-        }
-        apperAfterDelete();
-        element('element').onclick = function(){
-            var end = new Date().getTime();            
-            var timeTaken = (end -start) /1000;
-            element('element').style.display = "none";
-            element('timeTaken').innerHTML = timeTaken + " S.";
-            apperAfterDelete();            
-        };
+};
+apperAfterDelete();
+element('element').onclick = function(){
+    var end = new Date().getTime();            
+    var timeTaken = (end -start) /1000;
+    
+    if (bestTime > timeTaken || bestTime == 0) {
+        bestTime = timeTaken;
+    } else if (timeTaken == bestTime) {
+        bestTime = bestTime
+    }
+    element('element').style.display = "none";
+    element('timeTaken').innerHTML = timeTaken + " S.";
+    element('bestTime').innerHTML = bestTime + " S.";
+    apperAfterDelete();            
+};
